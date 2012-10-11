dd973e13ef6123b6764e279a3a969e69496a1645
Charles Ruhland <cruhland@alumni.cmu.edu>
2012-10-11 01:00:06 -0700

    Rough draft of list subscript question.

--- question_templates.py
+++ question_templates.py
@@ -2,6 +2,7 @@
 import data_generators as gen
 from user_interface import *
 import random
+import ast
 
 def type_of_literal():
     literal_gens = {
@@ -23,4 +24,18 @@ def type_of_literal():
         return answer == literal_type
     return question
 
-templates = (type_of_literal,)
+def list_subscripting():
+    a_list = gen.any_list()
+    index = random.randrange(len(a_list))
+    message = "Let some_list = %r. " + \
+              "Select the element at index %d in some_list."
+    def question():
+        show_text(message % (a_list, index))
+        answer = raw_input(">>> ")
+        parsed_answer = ast.parse(answer)
+        list_name_correct = parsed_answer.body[0].value.value.id == 'some_list'
+        list_index_correct = parsed_answer.body[0].value.slice.value.n == index
+        return list_name_correct and list_index_correct
+    return question
+
+templates = (type_of_literal, list_subscripting)
