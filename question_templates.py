bbee3960fce68f492004d7117ca212460f1e32ad
Charles Ruhland <cruhland@alumni.cmu.edu>
2012-10-12 13:01:45 -0700

    Added ordinal positions to list subscripting question.

--- question_templates.py
+++ question_templates.py
@@ -28,15 +28,30 @@ def list_subscripting():
     list_var = gen.str_gen.next()
     a_list = gen.make_filter(lambda seq: len(seq) > 0)(gen.list_gen).next()
     index = random.randrange(len(a_list))
-    message = "Let {var} = {value}. " + \
-              "Select the element at index {index} in {var}."
+    position = random.choice([index_position, ordinal_position])(index)
+    message = "Let {var} = {value}. Select the {position} in {var}."
+
     def question():
-        show_text(message.format(var=list_var, value=a_list, index=index))
+        text = message.format(var=list_var, value=a_list, position=position)
+        show_text(text)
         answer = raw_input(">>> ")
         parsed_answer = ast.parse(answer)
         list_name_correct = parsed_answer.body[0].value.value.id == list_var
         list_index_correct = parsed_answer.body[0].value.slice.value.n == index
         return list_name_correct and list_index_correct
     return question
+
+def index_position(index):
+    return "element at index {index}".format(index=index)
+
+def ordinal_position(index):
+    n = index + 1
+    return "{n}{suffix} element".format(n=n, suffix=ordinal_suffix(n))
+
+def ordinal_suffix(n):
+    if 11 <= n % 100 <= 13:
+        return "th"
+    else:
+        return {1: "st", 2: "nd", 3: "rd"}.get(n % 10, "th")
 
 templates = (type_of_literal, list_subscripting)
