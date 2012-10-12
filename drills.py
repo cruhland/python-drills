01623f044205fadafe091af1f7624cad855810b6
Charles Ruhland <cruhland@alumni.cmu.edu>
2012-10-12 12:08:19 -0700

    Use ^D to skip a question, ^C to quit.

--- drills.py
+++ drills.py
@@ -4,17 +4,28 @@ import random
 import question_templates
 
 def main():
+    print
+    print "Python drills"
+    print "At any time, press ^D to skip a question, or ^C to quit."
+
     while True:
         template = random.choice(question_templates.templates)
         question = template()
         while True:
             print
-            answered_correctly = question()
-            if answered_correctly:
-                print "*** CORRECT ***"
+            try:
+                answered_correctly = question()
+                if answered_correctly:
+                    print "*** CORRECT ***"
+                    break
+                else:
+                    print "Incorrect, try again"
+            except EOFError:
+                print "\nSkipping..."
                 break
-            else:
-                print "Incorrect, try again"
+            except KeyboardInterrupt:
+                print "\nQuitting..."
+                return
 
 if __name__ == '__main__':
     main()
